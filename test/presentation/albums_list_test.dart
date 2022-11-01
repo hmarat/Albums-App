4911b7be0e5bacda53f406a112f292e2ee8ea395
Marat Hakobjanyan <marat.hakobjanyan9@gmail.com>
2022-11-02 01:31:25 +0400

    refactor: remove unused import

--- test/presentation/albums_list_test.dart
+++ test/presentation/albums_list_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:albums/features/albums/presentation/widgets/albums_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
